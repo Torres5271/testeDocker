d6b4b6ccfe7415d191b6bbf74f157635a370b3d0
Torres <gustavo.souza@sptech.school>
2023-10-24 16:47:56 -0300

    6

--- docker.sh
+++ docker.sh
@@ -1,17 +1,33 @@
 #!/bin/bash
 
-echo "$(tput setaf 5)[Instalador DataSync]: $(tput sgr0)$(tput setaf 10)Olá, te ajudarei a instalar nossa aplicação!"
+# Cores para as mensagens (opcional)
+VERDE="\e[32m"
+AMARELO="\e[33m"
+NORMAL="\e[0m"
+
+# Função para exibir mensagens com cor
+print_msg() {
+    echo -e "$1$2$NORMAL"
+}
+
+print_msg "$VERDE" "[Instalador DataSync]: Olá, te ajudarei a instalar nossa aplicação!"
 sleep 2
 
-echo "$(tput setaf 5)[Instalador DataSync]: $(tput sgr0)$(tput setaf 10)Primeiro irei atualizar os pacotes do seu sistema."
+print_msg "$VERDE" "[Instalador DataSync]: Primeiro, vou atualizar os pacotes do seu sistema."
 sleep 2
 
-echo "$(tput setaf 5)[Instalador DataSync]: $(tput sgr0)$(tput setaf 10)Se o sistema operacional que você está utilizando for linux, será necessário informar sua senha SUDO agora."
-sleep 2
+# Verifica se o sistema é baseado no Debian (como o Ubuntu)
+if [ -x "$(command -v apt)" ]; then
+    sudo apt update -y
+    sudo apt upgrade -y
+elif [ -x "$(command -v dnf)" ]; then
+    sudo dnf update -y
+else
+    print_msg "$AMARELO" "[Instalador DataSync]: Sistema não suportado. Verifique o gerenciador de pacotes do seu sistema."
+    exit 1
+fi
 
-sudo apt upgrade && sudo apt update -y
-
-echo "Instalando o Docker..."
+print_msg "$VERDE" "[Instalador DataSync]: Instalando o Docker..."
 if ! command -v docker &> /dev/null; then
     if [ -x "$(command -v apt)" ]; then
         sudo apt update -y
@@ -21,13 +37,41 @@ if ! command -v docker &> /dev/null; then
     elif [ -x "$(command -v yum)" ]; then
         sudo yum install -y docker
     else
-        echo "Não foi possível instalar o Docker. Verifique o gerenciador de pacotes do seu sistema."
+        print_msg "$AMARELO" "[Instalador DataSync]: Não foi possível instalar o Docker. Verifique o gerenciador de pacotes do seu sistema."
         exit 1
     fi
 fi
-sudo docker run -d --safesync -e MYSQL_USER=aluno -e MYSQL_PASSWORD=sptech -p 3306:3306 mysql:latest
 
-sudo docker exec -i meu-banco mysql -u aluno -p < comandos.sql
+# Inicializa e habilita o serviço Docker
+sudo systemctl start docker
+sudo systemctl enable docker
+
+# Instalação do Java
+print_msg "$VERDE" "[Instalador DataSync]: Verificando se o Java está instalado..."
+if ! command -v java &> /dev/null; then
+    print_msg "$AMARELO" "Java não instalado."
+    read -p "Gostaria de instalar o Java? (s/n) " installJava
+    if [ "$installJava" == "s" ]; then
+        print_msg "$VERDE" "Iniciando a instalação do Java..."
+        if [ -x "$(command -v apt)" ]; then
+            sudo apt install openjdk-17-jre -y
+        elif [ -x "$(command -v dnf)" ]; then
+            sudo dnf install java-17-openjdk -y
+        else
+            print_msg "$AMARELO" "[Instalador DataSync]: Não foi possível instalar o Java. Verifique o gerenciador de pacotes do seu sistema."
+            exit 1
+        fi
+    else
+        print_msg "$AMARELO" "[Instalador DataSync]: Java é necessário para a aplicação. A instalação foi cancelada."
+        exit 1
+    fi
+fi
+
+# Criação do banco de dados e tabelas
+print_msg "$VERDE" "[Instalador DataSync]: Criando o banco de dados e tabelas..."
+mysql -u aluno -psptech <<EOF
+CREATE DATABASE IF NOT EXISTS safesync;
+USE safesync;
 
 CREATE TABLE IF NOT EXISTS empresas (
     idEmpresa INT PRIMARY KEY AUTO_INCREMENT,
@@ -54,34 +98,34 @@ CREATE TABLE IF NOT EXISTS funcionarios (
     FOREIGN KEY (fkEmpresa) REFERENCES empresas(idEmpresa)
 );
 
-create table if not exists hardwares(
-    id int primary key auto_increment,
-    sistemaOperacional varchar(50),
-    totalCpu double not null,
-    totalDisco double NOT NULL,
-    totalRam double not null,
-    fkFuncionario int,
-    foreign key (fkFuncionario) references funcionarios(idFuncionario)
+CREATE TABLE IF NOT EXISTS hardwares (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    sistemaOperacional VARCHAR(50),
+    totalCpu DOUBLE NOT NULL,
+    totalDisco DOUBLE NOT NULL,
+    totalRam DOUBLE NOT NULL,
+    fkFuncionario INT,
+    FOREIGN KEY (fkFuncionario) REFERENCES funcionarios(idFuncionario)
 );
 
-create table volateis(
-    id int primary key auto_increment,
-    consumoCpu double not null,
-    consumoDisco double not null,
-    consumoRam double not null,
-    totalJanelas int not null,
-    dataHora datetime,
-    fkHardware int,
-    foreign key (fkHardware) references hardwares(id)
+CREATE TABLE IF NOT EXISTS volateis (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    consumoCpu DOUBLE NOT NULL,
+    consumoDisco DOUBLE NOT NULL,
+    consumoRam DOUBLE NOT NULL,
+    totalJanelas INT NOT NULL,
+    dataHora DATETIME,
+    fkHardware INT,
+    FOREIGN KEY (fkHardware) REFERENCES hardwares(id)
 );
 
-create table if not exists limitador(
-    id int primary key auto_increment,
-    tipoComponente varchar(45),
-    min int,
-    max int,
-    fkEmpresa int not null,
-    foreign key (fkEmpresa) references empresas(idEmpresa)
+CREATE TABLE IF NOT EXISTS limitador (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    tipoComponente VARCHAR(45),
+    min INT,
+    max INT,
+    fkEmpresa INT NOT NULL,
+    FOREIGN KEY (fkEmpresa) REFERENCES empresas(idEmpresa)
 );
 
 CREATE TABLE IF NOT EXISTS arquivos (
@@ -91,75 +135,27 @@ CREATE TABLE IF NOT EXISTS arquivos (
     tamanho_arquivo INT NOT NULL,
     dados_arquivo LONGBLOB NOT NULL,
     data_upload TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    fkFuncionario INT not null,
-    FOREIGN KEY (fkfuncionario) REFERENCES funcionarios(idFuncionario)
+    fkFuncionario INT NOT NULL,
+    FOREIGN KEY (fkFuncionario) REFERENCES funcionarios(idFuncionario)
 );
 
+EOF
 
-
-
-echo "$(tput setaf 5)[Instalador DataSync]: $(tput sgr0) $(tput setaf 10)Para utilizar nossa aplicação é necessário ter o Java instalado"
+# Instalação do DataSync
+print_msg "$VERDE" "[Instalador DataSync]: Iniciando a instalação do DataSync..."
 sleep 2
 
-echo "$(tput setaf 5)[Instalador DataSync]: $(tput sgr0) $(tput setaf 10)Irei verificar se você já tem o Java."
-sleep 2
-
-# java -version 2>&1 | grep "openjdk" > /dev/null
-if [ $? -eq 0 ]
-then
-    echo "$(tput setaf 5)[Instalador DataSync]: $(tput sgr0)$(tput setaf 10)Você já tem o Java instalado!"
+# Verifica se o repositório DataSync já existe
+if [ -d "DataSync2.0" ]; then
+    print_msg "$VERDE" "[Instalador DataSync]: O repositório DataSync2.0 já existe. Atualizando..."
+    cd DataSync2.0
+    git pull
+    cd ..
 else
-    echo "Java não instalado." # Exibe a mensagem no terminal
-    echo "Gostaria de instalar o Java? [s/n]" # Exibe a mensagem no terminal
-    read get # Variável que guarda a resposta do usuário
-    if [ "$get" == "s" ]; then
-        echo "$(tput setaf 5)[Instalador DataSync]: $(tput sgr0)$(tput setaf 10)Iniciando a instalação do Java..."
-        sudo apt install openjdk-17-jre -y
-    fi
-fi
-sleep 2
-
-echo "$(tput setaf 5)[Instalador DataSync]: $(tput sgr0)$(tput setaf 10)Então iremos prosseguir com a instalação da DataSync..."
-sleep 2
-
-echo "$(tput setaf 5)[Instalador DataSync]: $(tput sgr0)$(tput setaf 10)Posso instalar a DataSync pra você? (Y/n)"
-read installDataSync
-if [ "$installDataSync" == "Y" ]
-then
-    echo "$(tput setaf 5)[Instalador DataSync]: $(tput sgr0)$(tput setaf 10)Irei iniciar a instalação..."
-    sleep 2
-
-    if [ -d "data-sync2" ]
-    then
-        echo "$(tput setaf 5)[Instalador DataSync]: $(tput sgr0)$(tput setaf 10)A pasta 'data-sync2' já existe. Atualizando..."
-        cd DataSync2.0
-        git pull
-        cd ..
-    else
-        git clone https://github.com/Safe-Sync/DataSync2.0.git
-        cd DataSync2.0/data-sync2
-    fi
-
+    git clone https://github.com/Safe-Sync/DataSync2.0.git
     cd DataSync2.0/data-sync2
-    cd target
+fi
 
-    chmod 777 data-sync2-1.0-jar-with-dependencies.jar
-    sleep 2
+cd DataSync2.0/data-sync2/target
 
-    echo "$(tput setaf 5)[Instalador DataSync]: $(tput sgr0)$(tput setaf 10)Obrigado por instalar a nossa aplicação!"
-    sleep 2
-
-    echo "$(tput setaf 5)[Instalador DataSync]: $(tput sgr0)$(tput setaf 10)Você quer executar a aplicação agora? (Y/n)"
-    read execDataSync
-    if [ "$execDataSync" == "Y" ]
-    then
-        echo "$(tput setaf 5)[Instalador DataSync]: $(tput sgr0)$(tput setaf 10)Iniciando a aplicação... Até logo!"
-        sleep 2
-        java -jar data-sync2-1.0-jar-with-dependencies.jar
-    else
-        echo "$(tput setaf 5)[Instalador DataSync]: $(tput sgr0)$(tput setaf 10)Você pode iniciar a aplicação quando desejar! Até logo!"
-        sleep 2
-        exit 0
-    fi
-else
-    echo "$(tput setaf 5)[Inst
+chmod
