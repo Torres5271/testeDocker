175676d1e110274826e9b23d9364044995500fb0
Torres <gustavo.souza@sptech.school>
2023-10-24 14:06:46 -0300

    3

--- docker.sh
+++ docker.sh
@@ -21,8 +21,15 @@ java --version
 if [ $? -eq 0 ]
 then
     echo "$(tput setaf 5)[Instalador DataSync]: $(tput sgr0)$(tput sgr0)$(tput setaf 10)Você já tem o Java instalado!"
+else #se nao,
+echo “java não instalado” #print no terminal
+echo “gostaria de instalar o java? [s/n]” #print no terminal
+read get #variável que guarda resposta do usuário
+if [ \“$get\” == \“s\” ]; #se retorno for igual a 0
+then #entao
 sudo apt install openjdk-17-jre -y #executa instalacao do java
 fi #fecha o 2º if
+fi #fecha o 1º if
     sleep 2
 
     echo "$(tput setaf 5)[Instalador DataSync]: $(tput sgr0)$(tput setaf 10)Então iremos prosseguir com a instalação da DataSync..."
